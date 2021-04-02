d7a29ce2115aa7266bab651bb50f46e20c3c7236
eimane <80257101+eimane@users.noreply.github.com>
2021-04-02 23:42:48 +0100

    Add files via upload

---
+++ 0x01-ssh/0-use_a_private_key.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#connecting to my server ubuntu using a private key
+ssh -i ~/.ssh/holberton ubuntu@34.73.169.180
---
+++ 0x01-ssh/1-create_ssh_key_pair.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#creating an RSA key pair
+ssh-keygen -t RSA -f holberton -b 4096 -N betty
